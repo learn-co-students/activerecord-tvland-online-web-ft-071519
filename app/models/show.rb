1ff30d9aeade1a03c7f96fcaf463eec15419b95c
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-19 22:22:12 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,14 +1,19 @@
+require 'pry'
+
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+  belongs_to :network
   
   def actors_list
     self.actors.map do|act|
       act.full_name
     end
   end
-def build_network(x)
+def build_network(call_letters:)
   
+ self.network = call_letters
+
 end  
 
 end
--- db/migrate/007_add_att_to_shows.rb
+++ db/migrate/007_add_att_to_shows.rb
@@ -6,6 +6,6 @@ class AddAttToShows < ActiveRecord::Migration[5.1]
     add_column :shows, :day, :string
     add_column :shows, :season, :string
     add_column :shows, :genre, :string
-    add_column :shows, :network_id, :integer
+    
   end
 end
